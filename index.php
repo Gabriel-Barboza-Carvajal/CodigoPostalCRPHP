2ca8515e933f0e77c114b8dd3350ec16d85de162
Visitante <Visitante@NicolasPC>
2020-12-22 18:44:13 -0600

    unos cambios para empezar a hacerlo responsivo

--- index.php
+++ index.php
@@ -10,13 +10,19 @@ and open the template in the editor.
         <script src="newjavascript.js" type="text/javascript"></script>
         <link href="default.css" rel="stylesheet" type="text/css"/>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!--BOSTRAP CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <!--BOSTRAP JS -->        
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
         <title>Código Postal CR</title>
     </head>
-      <body>
+    <body>
 
-        <div id="wrapper">
-            <header></header>
-            <div id="contents">
+        <div id="wrapper" class="">
+            <div id="contents" class="">
                 <div id="menu">
                     <section>
                         <p><strong><h2>Menú para Código Postal</h2></strong></p>
@@ -34,16 +40,12 @@ and open the template in the editor.
                         </select>
                     </section>
                 </div>
-                <div id="MostrarCodigo">
-                    <p >
-                        <strong>El Codigo es:</strong>
-                        <strong><span id="codigo">(No hay ninguna opción seleccionada)</span></strong>
-                    </p>
-                </div>               
+
+                <p id="MostrarCodigo">
+                    <strong>El Codigo es:</strong>
+                    <strong><span id="codigo">(No hay ninguna opción seleccionada)</span></strong>
+                </p>           
             </div>
-            <footer>
-                
-            </footer>
         </div>
     </body>
 </html>
