8298e38a32afb3c9021784e03d8c19cc3065c1c0
gabri <gabri@DESKTOP-JRMA0U1>
2021-02-06 18:29:19 -0600

    recortando el formulario de tamaño

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ and open the template in the editor.
                 <div class="container-fluid">
                     <div class="row">
                         <!-- left column -->
-                        <div class="col-md-6 formulario">
+                        <div class="col-md-3 formulario">
                             <!-- general form elements -->
                             <div class="card card-primary">
                                 <div class="card-header">
