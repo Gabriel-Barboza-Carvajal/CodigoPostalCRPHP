40c4fe7e19a014e1e9b684cf5145db61c77cc39e
gabri <gabri@DESKTOP-JRMA0U1>
2021-02-06 18:28:56 -0600

    cambios para probar la version responsiva

--- index.php
+++ index.php
@@ -10,38 +10,85 @@ and open the template in the editor.
         <script src="newjavascript.js" type="text/javascript"></script>
         <link href="default.css" rel="stylesheet" type="text/css"/>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <!--BOSTRAP CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <!--BOSTRAP JS -->        
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
+        <link href="estilos.css" rel="stylesheet" type="text/css"/>
+        <!-- Tell the browser to be responsive to screen width -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+        <!-- Ionicons -->
+        <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+        <!-- Tempusdominus Bbootstrap 4 -->
+        <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+        <!-- iCheck -->
+        <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+        <!-- JQVMap -->
+        <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+        <!-- Theme style -->
+        <link rel="stylesheet" href="dist/css/adminlte.min.css">
+        <!-- overlayScrollbars -->
+        <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+        <!-- Daterange picker -->
+        <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+        <!-- summernote -->
+        <link rel="stylesheet" href="plugins/summernote/summernote-bs4.css">
+        <!-- Google Font: Source Sans Pro -->
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">      
         <title>Código Postal CR</title>
     </head>
     <body>
+        <div class="wrapper">
+            <!--<div class="content-wrapper">-->
+            <!-- Main content -->
+            
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <!-- left column -->
+                        <div class="col-md-6 formulario">
+                            <!-- general form elements -->
+                            <div class="card card-primary">
+                                <div class="card-header">
+                                    <h3 class="card-title">Menú para Código Postal</h3>
+                                </div>
+                                <!-- /.card-header -->
+                                <!-- form start -->
+                                <form role="form">
+                                    <div class="card-body">
+                                        <div class="form-group">
+                                            <label for="MenuProvincia">Provincia : </label>
+                                            <select class="form-control" id="MenuProvincia"  onchange="filtrarMenuCanton()">
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="MenuCanton">Canton : </label>
+                                            <select class="form-control"  id="MenuCanton" onchange="filtrarMenuDistrito()"  >
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="MenuDistrito">Distrito : </label>
+                                            <select class="form-control"   id="MenuDistrito" onchange="calcularCodigo()"  >
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <!-- /.card-body -->
 
-        <div id="wrapper" class="container">
-                  
-                        <h2>Menú para Código Postal</h2>
-                        <p>Provincia : </p>
-                        <select id="MenuProvincia" onchange="filtrarMenuCanton()">
+                                    <div class="card-footer">
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    </div>
+                                </form>
+                                <p >
+                                    El Codigo es:
+                                    <span id="codigo">(No hay ninguna opción seleccionada)</span>
+                                </p>   
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </section>
+            <!-- /.content -->
+            <!--</div>-->
+        </div>
 
-                        </select>
-                        <p>Canton : </p>
-                        <select id="MenuCanton" onchange="filtrarMenuDistrito()" >
-
-                        </select>
-                        <p >Distrito : </p>
-                        <select id="MenuDistrito" onchange="calcularCodigo()">
-
-                        </select>
-                
-
-                <p >
-                   El Codigo es:
-                    <span id="codigo">(No hay ninguna opción seleccionada)</span>
-                </p>           
-           </div>
     </body>
 </html>
